c7e1cb36642ad71dd43bdd1d26e0707c2cd2a6b8
Knuspermann <knuspermann@proton.me>
2022-11-10 13:20:33 +0100

    ä+

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+with open("reimerei0.txt") as looplenght:
+    wörterliste = looplenght.read().split("\n")
+#print(wörterliste)
+for x in wörterliste:
+    if wörterliste.count(x) > 1:
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
